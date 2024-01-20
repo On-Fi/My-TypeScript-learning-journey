def066203a60a1bdc260bb787f712af9bde73f83
onur fistikci <onurfistikci@mail.de>
2024-01-20 18:03:37 +0100

    Lession #01

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import { Great } from './components/Great';
 
 function App() {
   return (
     <div className="App">
-      
+      <Great />
     </div>
   );
 }
---
+++ my-app/src/components/Great.tsx
@@ -0,0 +1,7 @@
+export const Great = () => {
+  return (
+    <div>
+      <h2>Moin Moin</h2>
+    </div>
+  );
+}
