77533d283bda707b8066a2ba18c5f5404f7e1b52
onur fistikci <onurfistikci@mail.de>
2024-01-25 20:27:10 +0100

    import input

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,7 @@ import { Status } from './components/Status';
 import { Heading } from './components/Heading';
 import { Oscar } from './components/Oscar';
 import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   const personName = {
@@ -23,6 +24,7 @@ function App() {
     <div className="App">
       <Heading>Welcome my friend! </Heading>
       <Button handleClick={(event, id) => console.log('Button clicked!', event, id)} />
+      <Input value='' handleChange={event => console.log(event)} />
       <Status status='error' />
       <Status status='loading' />
       <Status status='success' />
--- my-app/src/components/Input.tsx
+++ my-app/src/components/Input.tsx
@@ -3,4 +3,5 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void; };
 
 export const Input = (props: InputProps) => { 
-    return <input type='text' value={props.value}  onChange={props.handleChange} /> }
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => { console.log(event); };
+    return <input type='text' value={props.value}  onChange={handleInputChange} /> }
